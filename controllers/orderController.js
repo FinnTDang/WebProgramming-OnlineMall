1a62a880d3455dc5aa87a5b4b6d11715f55974b5
Trung Le <s3979718@rmit.edu.vn>
2024-04-28 21:02:06 +0700

    Add a controller function to orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,11 @@
 const Order = require("../models/order");
 const asyncHandler = require("express-async-handler");
 
+//Display list of all Order on GET
+exports.order_list = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: List of all orders");
+});
+
 //Display Order on GET
 exports.order_display = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Display order");
