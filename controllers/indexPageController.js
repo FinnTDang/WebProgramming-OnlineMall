edfd1e2432fd13148dd50b4eab2077e893c665f0
dophuonglinhh <s3926823@rmit.edu.vn>
2024-05-05 23:52:20 +0700

    [homepage] homepage frontend

--- controllers/indexPageController.js
+++ controllers/indexPageController.js
@@ -8,25 +8,12 @@ exports.display_index = asyncHandler(async (req, res, next) => {
   // const new_products;
   // const featured_stores;
   // const featured_products;
-  const new_stores = [
-    { url: "/store-1", logo: "/logo1.png", name: "Store One" },
-    { url: "/store-2", logo: "/logo2.png", name: "Store Two" }
-  ];
 
-  const new_products = [
-    { url: "/product-1", image: "/image1.png", name: "Product One", store_url: "/store-1", store_name: "Store One", price: "$19.99" },
-    { url: "/product-2", image: "/image2.png", name: "Product Two", store_url: "/store-2", store_name: "Store Two", price: "$29.99" }
-  ];
-
-  const featured_stores = [
-    { url: "/store-1", logo: "/logo1.png", name: "Store One" },
-    { url: "/store-2", logo: "/logo2.png", name: "Store Two" }
-  ];
-
-  const featured_products = [
-    { url: "/product-1", image: "/image1.png", name: "Product One", store_url: "/store-1", store_name: "Store One", price: "$19.99" },
-    { url: "/product-2", image: "/image2.png", name: "Product Two", store_url: "/store-2", store_name: "Store Two", price: "$29.99" }
-  ];
+  // fetch data from db
+  const new_stores = await Store.find({}).limit(0); 
+  const new_products = await Product.find({}).limit(0);
+  const featured_stores = await Store.find({}).limit(0);
+  const featured_products = await Product.find({}).limit(0);
 
   res.render('index', { 
     new_stores: new_stores, 
